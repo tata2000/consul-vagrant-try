eee78d378053e47d5f0721f533ca325dd313d6f7
Tatyana Arenburg <tatyana@getbrew.com>
2023-05-17 23:07:32 +0100

    finished here

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,7 +2,7 @@
 
 CONSUL_ADDR=${CONSUL_ADDR:-consul:8500}
 # Start Consul agent in the background
-consul agent -dev &
+consul agent -config-dir=/etc/consul.d/ -bind=$(ip -o -4 addr list $CONSUL_BIND_INTERFACE | head -n1 | awk '{print $4}' | cut -d/ -f1) &
 
 # Start Consul-Template in the background
 consul-template -consul-addr=${CONSUL_ADDR} -template="${NGINX_CTMPL}:/etc/nginx/nginx.conf:service nginx reload" &
